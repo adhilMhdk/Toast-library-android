cf789e8f6c8bef3a5ca4f731750f52b83dbec6da
adhil <adhil@example.com>
2020-12-11 20:36:35 +0530

    Second

--- ToastLib/src/main/java/com/toastlib/DoToast.java
+++ ToastLib/src/main/java/com/toastlib/DoToast.java
@@ -3,12 +3,8 @@ package com.toastlib;
 import android.app.Activity;
 
 public class DoToast {
-    Activity activity;
 
-    public DoToast(Activity activity) {
-        this.activity = activity;
-    }
-    public void show(String message){
+    public static void show(Activity activity,String message){
         android.widget.Toast.makeText(activity, message, android.widget.Toast.LENGTH_LONG).show();
     }
 }
